c383041b00efbeb4bc0d22abedf91bc9d2cd1f0c
IsaacHenry422 <henry42122@gmail.com>
2025-05-06 13:34:50 +0100

    Update app.js, package.json, package-lock.json, and config

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
+const path = require('node:path'); // Import the path module
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+
+//console.log('Inside config.js, MONGODB_URI from process.env:', process.env.MONGODB_URI);
 
 module.exports = {
-    PORT: process.env.PORT || 5000, // Add a default port if not in .env
+    PORT: process.env.PORT || 4000,
     MONGODB_URI: process.env.MONGODB_URI,
-    SESSION_SECRET: process.env.SESSION_SECRET, // Important for session security
+    SESSION_SECRET: process.env.SESSION_SECRET,
     PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY || 'yourActualPaystackSecretKey',
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || 'yourActualCloudinaryCloudName',
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || 'yourActualCloudinaryApiKey',
@@ -12,6 +15,7 @@ module.exports = {
     EMAIL_PASS: process.env.EMAIL_PASS || 'yourActualEmailPassword',
     MAILGUN_API_KEY: process.env.MAILGUN_API_KEY || 'yourActualMailgunApiKey',
     MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN || 'yourMailgunDomain.com',
-    FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000', // For CORS and email links
-    NODE_ENV: process.env.NODE_ENV || 'development', // To determine environment
+    FRONTEND_URL: process.env.FRONTEND_URL || 'https://crownlist-staging.vercel.app/',
+    NODE_ENV: process.env.NODE_ENV || 'production',
+    SESSION_SECRET: process.env.SESSION_SECRET
 };
